0fb4985af0ce48f432913c075f6b0888b4661aa4
Anish Sahoo <sahoo.an@northeastern.edu>
2025-10-09 18:09:18 -0400

    Add linting fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,10 @@ import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import cors from "cors";
 import express from "express";
 import { connectRedis } from "./data/db/redis.js";
+import { connectPostgres } from "./data/db/sql.js";
 import { appRouter } from "./trpc/app_router.js";
 import log from "./utils/logger.js";
 import { registerTrpcUiRoute } from "./utils/trpc-ui.js";
-import { connectPostgres } from "./data/db/sql.js";
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
