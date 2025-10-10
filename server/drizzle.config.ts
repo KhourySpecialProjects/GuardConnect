a332867399df1da8b20a20a95fbeda13ddca1549
milomargolis <margolis.mi@northeastern.edu>
2025-10-10 09:34:42 -0400

    adding initila schema and drizzle setup

---
+++ server/drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/data/db/schema",
+  out: "./src/data/db/migrations",
+});
+
+
