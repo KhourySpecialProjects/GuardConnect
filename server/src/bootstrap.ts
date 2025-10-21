1a72a0acf2b5f07a72dec1f32d400194453d52b5
Thomas Young <thomasjonyoung@gmail.com>
2025-10-21 08:30:57 -0400

    changes

--- server/src/bootstrap.ts
+++
@@ -1,8 +0,0 @@
-import { config } from "dotenv";
-
-// load the env first
-config({ path: "../.env", quiet: true });
-config({ path: ".env", quiet: true });
-
-// then start the rest of the server startup
-await import("./index.js");
--- web/next.config.ts
+++ web/next.config.ts
@@ -1,14 +1,5 @@
-import { config } from "dotenv";
 import type { NextConfig } from "next";
 
-// load in the root config as well
-config({ path: "../.env", quiet: true });
-
-const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.API_BASE_URL,
-    NEXT_PUBLIC_WEB_BASE_URL: process.env.WEB_BASE_URL,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
