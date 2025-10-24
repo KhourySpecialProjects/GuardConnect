1b5ee93259e96cb54e1d928b611516f3e7fd4207
oliviaseds <osedarski@gmail.com>
2025-10-24 08:06:40 -0400

    formatting

--- web/src/components/dropdown-select.tsx/index.tsx
+++ web/src/components/dropdown-select.tsx/index.tsx
@@ -19,18 +19,20 @@ export const DropdownSelect = ({
 }) => {
   return (
     <Select value={value} onValueChange={onChange}>
-      <SelectTrigger className="hover:data-[placeholder]:text-white text-subheader font-semibold py-5 rounded-xl bg-white hover:bg-primary hover:text-white border-2 border-primary w-[332px]">
+      <SelectTrigger className="hover:data-[placeholder]:text-white data-[state=open]:rounded-b-none data-[state=open]:border-b-0 text-subheader font-semibold py-5 rounded-xl bg-white hover:bg-primary hover:text-white border-2 border-primary w-[332px]">
         <SelectValue
           placeholder={
             options.length > 0 ? options[0].label : "Select an option"
           }
         />
       </SelectTrigger>
-      <SelectContent>
+      <SelectContent className="rounded-b-xl rounded-t-none w-[var(--radix-select-trigger-width)] border-2 border-primary">
         <SelectGroup>
-          {options.map((option) => (
+          {options.map((option, index) => (
             <SelectItem
-              className="text-subheader font-semibold text-primary py-3"
+              className={`text-subheader font-semibold text-primary py-3 rounded-none ${
+                index > 0 ? "border-t-2 border-primary" : ""
+              }`}
               key={option.value}
               value={option.value}
             >
